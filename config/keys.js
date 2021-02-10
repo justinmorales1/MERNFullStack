b6521992adb7780e1ca4f3653a086dc92b5832c1
justinmorales1 <justin@justins-mbp.lan>
2021-02-09 18:15:56 -0600

    Adding gitignore

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+    clientId : '628851259334-n61hkntd5jlcbjbk4fou51vspg984e4d.apps.googleusercontent.com',
+    secretId : 'p1Mxd8HpdWqKOmzHgNRow3v2'
+}
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-//Creating route handler and associating it with a given route
 
+passport.use(new GoogleStrategy(clientId, secretId));
+
+//Creating route handler and associating it with a given route
 app.get('/', (req, res)=> {
     res.send({bye: 'buddy'});
 
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
