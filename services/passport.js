fe75e5f7a89fbfdaae1abaffd9d349bae28e7544
justinmorales1 <justin@justins-mbp.lan>
2021-02-10 21:32:35 -0600

    Updated prod and dev values

--- services/passport.js
+++ services/passport.js
@@ -25,7 +25,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID : keys.googleClientID,
     clientSecret: keys.googleSecretID,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
 }, (accessToken, refreshToken, profile, done) => {
     User.findOne({googleId: profile.id}).then((existingUser)=> {
        if(existingUser) {
