721c98eb0148611f86e08e98edd276a0a4f15e26
justinmorales1 <justin@justins-mbp.lan>
2021-02-10 21:21:18 -0600

    Updated prod and dev values

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID : '628851259334-n61hkntd5jlcbjbk4fou51vspg984e4d.apps.googleusercontent.com',
+    googleSecretID : 'p1Mxd8HpdWqKOmzHgNRow3v2',
+    mongoURI: 'mongodb+srv://jmora260:SksNv9VybN1YPfuy@surveyme.dsyiu.mongodb.net/SurveyMe?retryWrites=true&w=majority',
+    cookieKey: 'kjladslflcelakslcjc;dm;lksadfc;lmas;ldfj;lsdkjfmcal;kms'
+}
---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID : process.env.GOOOGLE_CLIENT_ID,
+    googleSecretID : process.env.GOOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
+
