0c7285bd11e901ea8e61e10f4d57ac771ab87c6c
justinmorales1 <justin@justins-mbp.lan>
2021-02-13 09:23:34 -0600

    Updating header, landing page, and added stripes payment package

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends React.Component {
+
+
+    renderContent () {
+        console.log(this.props.auth)
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li>
+                        <a href="/auth/google"> Login With Google </a>
+                    </li>
+                )
+            default:
+                return <li><a href="/api/logout">Logout</a></li>
+        }
+    }
+
+
     render() {
+
+        console.log(this.props.auth)
         return (
                 <nav>
                     <div className="nav-wrapper">
-                        <a href="#" className="left brand-logo"> Survey Me </a>
+                        <Link to={this.props.auth ? '/surveys' : '/'}
+                              className="left brand-logo"> Survey Me
+                        </Link>
                         <ul id="nav-mobile" className="right">
-                            <li><a href="sass.html">Login With Google</a></li>
+                            <li>
+                                {this.renderContent()}
+                            </li>
                         </ul>
                     </div>
                 </nav>
@@ -15,5 +42,11 @@ class Header extends React.Component {
     }
 }
 
+const mapStateToProps = ({auth}) => {
+    return {
+        auth
+    }
+}
 
-export default Header;
+
+export default connect(mapStateToProps)(Header);
