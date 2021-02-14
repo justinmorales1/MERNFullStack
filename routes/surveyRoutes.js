f96da0007cdc44f8e9378569562023920debfab2
justinmorales1 <jm.em0210@gmail.com>
2021-02-14 08:28:19 -0600

    Updating pakage json, middleswares, and configs

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,12 +1,25 @@
+const mongoose = require('mongoose');
 const requirelogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+
+const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.post('/api/surveys', requirelogin, (req, res) => {
+    app.post('/api/surveys', requirelogin, requireCredits, (req, res) => {
+        const { title, subject, body, recipients } = req.body;
+
+
+        const survey = new Survey({
+            title,
+            body,
+            subject,
+            recipients : recipients.split(',').map((email) => {
+                return { email: email.trim() };
+            }),
+            _user : req.user.id,
+            dateSent: Date.now(),
+        })
+
 
     });
-
-
-
-
-
 };
